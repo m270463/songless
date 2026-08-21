117d0da4bebd2a3bc02b62d82a5bfa0337e9ce5a
Matheus Assis <m270463@dac.unicamp.com.br>
2026-08-21 20:46:50 -0300

    commit inicial

---
+++ src/main/java/com/matheus/songless/SonglessApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.songless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonglessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonglessApplication.class, args);
+	}
+
+}
