83cbabb4446abf20b0558b12690b8f6244db6eed
Matheus Assis <m270463@dac.unicamp.com.br>
2026-08-23 14:30:02 -0300

    cria o mecanismo de busca e o controller com endpoint para o navegador

---
+++ src/main/java/com/matheus/songless/AutoCompleteController.java
@@ -0,0 +1,21 @@
+package com.matheus.songless;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController          
+public class AutoCompleteController {
+    private final BuscaAutoComplete buscaAutoComplete;
+
+    public AutoCompleteController(BuscaAutoComplete buscaAutoComplete){
+        this.buscaAutoComplete = buscaAutoComplete;
+    }
+
+    @GetMapping("/api/autocomplete")   
+    public ArrayList<String> autocomplete(@RequestParam String termo) {
+        return buscaAutoComplete.buscaComplete(termo);
+    }
+}
