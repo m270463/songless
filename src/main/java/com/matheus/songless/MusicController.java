73baa965036173bb56efff1495b25925f9136425
Matheus Assis <m270463@dac.unicamp.com.br>
2026-08-29 20:29:40 -0300

    remove as repetições de música durante uma mesma sessão de jogo

--- src/main/java/com/matheus/songless/MusicController.java
+++ src/main/java/com/matheus/songless/MusicController.java
@@ -1,5 +1,7 @@
 package com.matheus.songless;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,14 @@ public class MusicController {
     
 
     @GetMapping("/api/musica")
-    public Musica sortear(@RequestParam String opcao,@RequestParam String artista){
-        return repositorio.escolherAleatoria(opcao,artista);
+    public Musica sortear(@RequestParam String opcao,@RequestParam String artista, @RequestParam (required = false) String excluir){
+        ArrayList<Integer> idsExcluidos = new ArrayList<>();
+                if (excluir != null && !excluir.isBlank()) {
+            for (String id : excluir.split(",")) {
+                idsExcluidos.add(Integer.parseInt(id.trim()));
+            }
+        }
+        return repositorio.escolherAleatoria(opcao, artista, idsExcluidos);
+
     }
 }
